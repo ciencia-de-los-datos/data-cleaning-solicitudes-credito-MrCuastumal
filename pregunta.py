6675d7220ec072b745c836e5cba7bba95b36247b
Carlos Cuastumal <carloscuastumal64@gmail.com>
2023-04-10 15:51:45 -0500

    Se sube modificacion del archivo csv y tests, ademas se realiza ejercicio

--- pregunta.py
+++ pregunta.py
@@ -12,22 +12,22 @@ import numpy as np
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df.replace(r'^\s*$', np.NaN, regex=True, inplace=True)
-    df.dropna(inplace=True)
-    df.drop(columns=["Unnamed: 0"], inplace=True)
-    df = df.astype({'sexo':'string', 'tipo_de_emprendimiento':'string', 'idea_negocio':'string', 'barrio':'string', 'línea_credito':'string'})
-    df = df.apply(lambda x: x.str.lower() if x.dtype == 'string' else x)
-    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: float(x.replace('$', '').replace(',', '')) if isinstance(x, str) else x)
-    for col in ["idea_negocio", "barrio", "línea_credito"]:
-        df[col] = (df[col]
-                .str.replace(r"[._]", " ", regex=True)
-                .str.replace(r"-", " ", regex=True)
-                .str.replace(r"\s+", " ", regex=True)
-                .str.normalize('NFKD')
-                .str.encode('ascii', errors='ignore')
-                .str.decode('utf-8'))
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col='Unnamed: 0')
+
+    df.sexo = df.sexo.str.lower()
+    df.sexo = df.sexo.astype('category')
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.astype('category')
+    df.idea_negocio = df.idea_negocio.str.lower().str.replace("_|-", " ", regex=True).astype('category')
+    df['barrio'] = df['barrio'].str.lower().astype('category').str.replace(r'_|-', ' ', regex=True)
+    df.estrato = df.estrato.astype('category')
+    df.comuna_ciudadano = df.comuna_ciudadano.astype('category')
+    df.fecha_de_beneficio = pd.to_datetime(df['fecha_de_beneficio'], dayfirst= True)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(r'[^\d.]', '', regex=True).astype(float)
+    df['línea_credito'] = df['línea_credito'].str.lower().astype('category')
+    df['línea_credito'] = df['línea_credito'].str.replace("_|-", " ", regex=True)
+
+    df.dropna(axis='index',inplace=True)
     df.drop_duplicates(inplace=True)
-    df.reset_index(drop=True, inplace=True)
 
     return df
